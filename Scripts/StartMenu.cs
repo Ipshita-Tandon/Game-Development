9cef842bc92530188b1a02088af1b0a526047b6a
Ipshita-Tandon <ipshitatandon@gmail.com>
2024-07-04 21:15:01 +0530

    Update StartMenu.cs

--- Scripts/StartMenu.cs
+++ Scripts/StartMenu.cs
@@ -10,32 +10,3 @@ public class StartMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
-
-
-
-// STICKY PLATFORM
-
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class StickyPlatform : MonoBehaviour
-{
-    private void OnTriggerEnter2D(Collider2D collision) {
-
-        if (collision.gameObject.name == "Player")
-        {
-            collision.gameObject.transform.SetParent(transform);    // player as a child of moving platfrom
-        }
-
-    }
-
-    private void OnTriggerExit2D(Collider2D collision) {
-
-        if (collision.gameObject.name == "Player")
-        {
-            collision.gameObject.transform.SetParent(null);    // player as a child of moving platfrom
-        } 
-
-    }
-}
